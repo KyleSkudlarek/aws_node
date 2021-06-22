97951865f7def751aff9e79dcb0bc22f3da99ed6
Kyle <kjs2wp@virginia.edu>
2021-06-22 01:38:19 -0500

    Updates 2

--- create_buckets.js
+++ create_buckets.js
@@ -1,19 +1,13 @@
-// Load the SDK and UUID
+// Load the AWS SDK
 var AWS = require('aws-sdk');
 
 
 // Create unique bucket name
-var bucketName = 'node-sdk-sample213';
+var bucketName = 'mybucket';
 
-// Create a promise on S3 service object
-var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
+s3 = new AWS.S3();
 
-// Handle promise fulfilled/rejected states
-bucketPromise.then(
-    function(data) {
-        // Create params for putObject call
-        console.log("Successfully created " + bucketName);
-    }).catch(
-    function(err) {
-        console.error(err, err.stack);
-    });
+s3.createBucket({Bucket: bucketName}, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else    console.log(data);           // successful response
+});
--- list_s3_buckets.js
+++ list_s3_buckets.js
@@ -1,18 +1,10 @@
+// Load the AWS SDK
 var AWS = require('aws-sdk');
 // Set the region
 AWS.config.update({region: 'us-east-1'});
 
 // Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-
-// s3.listBuckets(function(err, data) {
-//     if (err) {
-//         console.log("Error", err);
-//     } else {
-//         console.log("Success", data.Buckets);
-//     }
-// });
+s3 = new AWS.S3();
 
 
 s3.listBuckets(function(err, data) {
